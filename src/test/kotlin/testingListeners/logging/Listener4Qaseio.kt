f1a4e253811ec6f1c7b37ad4230f09f110d63802
thoth291 <anaryusifov@gmail.com>
2022-04-12 09:32:00 -0500

    Now added a response to my setup call from restassured. Fixed yaml parser being depreciated. This is dummy setup - in reality the code should be organized differently.

--- src/test/kotlin/testingListeners/logging/Listener4Qaseio.kt
+++ src/test/kotlin/testingListeners/logging/Listener4Qaseio.kt
@@ -5,33 +5,34 @@ import org.testng.ITestContext
 import org.testng.ITestResult
 import org.testng.TestListenerAdapter
 
-class Listener4qaseio : TestListenerAdapter() {
+class Listener4Qaseio : TestListenerAdapter() {
 
     private val client = QaseioClient()
 
     override fun onStart(context: ITestContext) {
-        client.setupClient()
+        val setupClient = client.setupClient()
+        Logger4Qaseio.log("RESPONCE: $setupClient")
         super.onStart(context)
-        Logger4qaseio.log("BEGIN: $context")
-        Logger4qaseio.log("Beginning test suite: ${context.outputDirectory}")
+        Logger4Qaseio.log("BEGIN: $context")
+        Logger4Qaseio.log("Beginning test suite: ${context.outputDirectory}")
     }
     override fun onFinish(context: ITestContext?) {}
     override fun onTestSkipped(result: ITestResult?) {}
     override fun onTestStart(result: ITestResult) {
         super.onTestStart(result)
-        Logger4qaseio.log("START: ${result.parameters.asSequence().map{it.toString()}.toList()}")
-        Logger4qaseio.log("CONTINUE: ${result.attributeNames}")
-        Logger4qaseio.log("===>>> Test started: ${result.name}")
+        Logger4Qaseio.log("START: ${result.parameters.asSequence().map{it.toString()}.toList()}")
+        Logger4Qaseio.log("CONTINUE: ${result.attributeNames}")
+        Logger4Qaseio.log("===>>> Test started: ${result.name}")
     }
     override fun onTestSuccess(result: ITestResult?) {
         super.onTestSuccess(result)
         if (result != null) {
-            Logger4qaseio.log("<<<=== Test completed successfully: ${result.name}")
+            Logger4Qaseio.log("<<<=== Test completed successfully: ${result.name}")
         }
     }
     override fun onTestFailure(result: ITestResult) {
         super.onTestFailure(result)
-        Logger4qaseio.log("<<<=== Test failed: ${result.name}")
+        Logger4Qaseio.log("<<<=== Test failed: ${result.name}")
     }
     override fun onTestFailedButWithinSuccessPercentage(result: ITestResult?) {}
 }
